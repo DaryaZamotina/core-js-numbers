5e4c4c8a5add3c80495812235af56e47c0df8b46
Darya Zamotina <133055781+DaryaZamotina@users.noreply.github.com>
2023-12-20 16:41:32 +0300

    feat: isPrime done

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -204,15 +204,14 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  /*
-  if (n <= 1 || (n % 2 == 0 && n !== 2)) return false;
+function isPrime(n) {
+  if (n <= 1) return false;
+  if (n % 2 === 0 && n > 2) return false;
 
-  for (let i = 3; i < Math.floor(Math.sqrt(n)) + 1; i + 2) {
-    if (n % i == 0) return false;
+  for (let i = 3; i < Math.sqrt(n) + 1; i += 2) {
+    if (n % i === 0) return false;
   }
   return true;
-  */
 }
 
 /**
